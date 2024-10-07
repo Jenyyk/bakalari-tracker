8f50aa07da8e5185a3620593f4dcbb995ce3708b
Jenyyk <jendotron2@gmail.com>
2024-10-07 18:39:16 +0200

    multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use dotenv::dotenv;
+use std::{
+    thread,
+    time::Duration
+};
 
 fn main() {
     dotenv().ok();
     let bakalari_url = std::env::var("BAKALARI_URL").expect("Chybí url v .env souboru");
+    let refresh_interval = Duration::from_millis(std::env::var("MS_SLEEP_BETWEEN_CHECKS")
+        .expect("Chybí čas spaní v .env souboru")
+        .parse::<u64>()
+        .unwrap_or(600000)
+    );
     println!("{bakalari_url}");
+    loop {
+        thread::spawn(|| {
+            println!("printing from thread") // debug
+        });
+        thread::sleep(refresh_interval);
+    }
 }
